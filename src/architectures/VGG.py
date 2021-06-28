05f43ddc2629c80e4a41bce32222190315189c05
Thiago Lermen <thiago.lermen@gmail.com>
2021-06-28 04:07:11 -0300

    add VGG architecture from scratch using PyTorch

--- src/architectures/VGG.py
+++ src/architectures/VGG.py
@@ -3,100 +3,83 @@ import torch.nn as nn
 
 
 VGG_types = {
-    "VGG11": [64, "M", 128, "M", 256, 256, "M", 512, 512, "M", 512, 512, "M"],
-    "VGG13": [64, 64, "M", 128, 128, "M", 256, 256, "M", 512, 512, "M", 512, 512, "M"],
+    "VGG11": [
+        64, "M",
+        128, "M",
+        256, 256, "M",
+        512, 512, "M",
+        512, 512, "M"
+    ],
+    "VGG13": [
+        64, 64, "M",
+        128, 128, "M",
+        256, 256, "M",
+        512, 512, "M",
+        512, 512, "M" 
+    ],
     "VGG16": [
-        64,
-        64,
-        "M",
-        128,
-        128,
-        "M",
-        256,
-        256,
-        256,
-        "M",
-        512,
-        512,
-        512,
-        "M",
-        512,
-        512,
-        512,
-        "M",
+        64, 64, "M",
+        128, 128, "M",
+        256, 256, 256, "M",
+        512, 512, 512, "M",
+        512, 512, 512, "M"
     ],
     "VGG19": [
-        64,
-        64,
-        "M",
-        128,
-        128,
-        "M",
-        256,
-        256,
-        256,
-        256,
-        "M",
-        512,
-        512,
-        512,
-        512,
-        "M",
-        512,
-        512,
-        512,
-        512,
-        "M",
-    ],
-}
+        64, 64, "M",
+        128, 128, "M",
+        256, 256, 256, 256, "M",
+        512, 512, 512, 512, "M",
+        512, 512, 512, 512, "M"
+    ]
+} 
 
-class VGG_net(nn.Module):
-  def __init__(self, in_channels=3, num_classes=1000):
-      super(VGG_net, self).__init__()
-      self.in_channels = in_channels
-      self.conv_layers = self.create_conv_layers(VGG_types["VGG16"])
-      self.fc1 = nn.Sequential(
-        nn.Linear(512*7*7, 4096),
-        nn.ReLU(),
-        nn.Dropout(p=0.5),
-        nn.Linear(4096, 4096),
-        nn.ReLU(),
-        nn.Dropout(p=0.5),
-        nn.Linear(4096, num_classes)
-      )
+class VGG_net(nn.Module): 
+    def __init__(self, in_channels=3, num_classes=1000): 
+        super(VGG_net, self).__init__()
+        self.in_channels = in_channels
+        self.conv_layers = self.create_conv_layers(VGG_types["VGG16"])
+        self.fc1 = nn.Sequential(
+            nn.Linear(512*7*7, 4096),
+            nn.ReLU(),
+            nn.Dropout(p=0.5),
+            nn.Linear(4096, 4096),
+            nn.ReLU(),
+            nn.Dropout(p=0.5),
+            nn.Linear(4096, num_classes)
+        )
       
   
-  def forward(self, x):
-    x = self.conv_layers(x)
-    x = x.reshape(x.shape[0], -1)
-    x = self.fc1(x)
-    return x
+    def forward(self, x):
+        x = self.conv_layers(x)
+        x = x.reshape(x.shape[0], -1)
+        x = self.fc1(x)
+        return x
 
-  def create_conv_layers(self, architecture):
-    layers = []
-    in_channels = self.in_channels
+    def create_conv_layers(self, architecture):
+        layers = []
+        in_channels = self.in_channels
 
-    for x in architecture:
-      if type(x) == int:
-        out_channles = x
+        for x in architecture:
+          if type(x) == int:
+            out_channles = x
 
-        layers += [
-          nn.Conv2d(
-            in_channels=in_channels, 
-            out_channels=out_channles, 
-            kernel_size=(3,3), 
-            stride=(1,1), 
-            padding=(1,1)
-          ), 
-          nn.ReLU()
-        ]
-        
-        in_channels = x
+            layers += [
+              nn.Conv2d(
+                in_channels=in_channels, 
+                out_channels=out_channles, 
+                kernel_size=(3,3), 
+                stride=(1,1), 
+                padding=(1,1)
+              ), 
+              nn.ReLU()
+            ]
+            
+            in_channels = x
 
-      elif x == 'M':
-        layers += [nn.MaxPool2d(kernel_size=(2,2), stride=(2,2))]
+          elif x == 'M':
+            layers += [nn.MaxPool2d(kernel_size=(2,2), stride=(2,2))]
 
-    return nn.Sequential(*layers)
+        return nn.Sequential(*layers)
 
 
 if __name__ == '__main__':
